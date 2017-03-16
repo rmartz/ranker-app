f82f14092befa4c338207fef0887ea41d7065413
Reed Martz <reed@reedmartz.com>
2017-03-15 22:18:57 -0400

    Learning Angular 2

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Topic {
+  id: number;
+  label: string;
+};
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{topic.label}}</h2>
+  <div><label>id: </label>{{topic.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="topic.label" placeholder="label">
+  </div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    name = 'Ranker';
+    topic: Topic = {
+        id: 1,
+        label: "Best Restaurants"
+    };
+}
