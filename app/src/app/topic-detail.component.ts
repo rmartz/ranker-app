4d0310e54db718623060d14f664093e488e895f4
Reed Martz <reed@reedmartz.com>
2017-07-03 20:46:09 -0400

    Show contests in topic detail

--- app/src/app/topic-detail.component.ts
+++ app/src/app/topic-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { TopicService } from './topic.service'
+import { ContestService } from './contest.service'
 import { Topic } from './topic'
 import { Option } from './option'
 
@@ -9,14 +10,17 @@ import { Option } from './option'
   templateUrl: 'app/topic-detail.component.html'
 })
 export class TopicDetailComponent {
-    constructor(private topicService: TopicService) { }
+    constructor(private topicService: TopicService, private contestService: ContestService) { }
 
     ngOnChanges(...args: any[]) {
         this.topicService.listTopicOptions(this.topic).subscribe(
             options => this.options = options)
+        this.contestService.get(this.topic).subscribe(
+            contest => this.contest = contest)
     }
 
     options: Option[];
+    contest: Option[];
 
     @Input()
     topic: Topic;
